c2eced586e5a35be172830f0f3d69b6feb214a4b
DimRev <dimarev444@gmail.com>
2024-08-21 17:05:17 +0300

    Implement clock and calc dt on each tick

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -13,6 +15,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
